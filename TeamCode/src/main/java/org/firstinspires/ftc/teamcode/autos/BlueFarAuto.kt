c91bac20cfe47fc291bfe5a1bd01afc23e535600
joshuafocht <joshua.focht@outlook.com>
2025-12-04 18:46:15 -0500

    add far autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueFarAuto.kt
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.autos
+
+import com.pedropathing.geometry.BezierCurve
+import com.pedropathing.geometry.Pose
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants
+
+
+class BlueFarAuto : LinearOpMode() {
+    override fun runOpMode() {
+        val follower = Constants.createFollower(hardwareMap)
+        follower.setStartingPose(Pose(56.000, 8.000, 90.0))
+        follower.update()
+
+        val path = follower
+            .pathBuilder()
+            .addPath(
+                BezierCurve(
+                    Pose(56.000, 8.000),
+                    Pose(48.000, 24.000),
+                    Pose(36.000, 8.000)
+                )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(90.0))
+            .build()
+
+        waitForStart()
+        follower.followPath(path)
+        while (opModeIsActive() && follower.followingPathChain) {
+            follower.update()
+        }
+    }
+}
